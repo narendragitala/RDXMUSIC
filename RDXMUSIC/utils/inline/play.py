acfc8442c83cc05b022603da4a8801c593e9bc3b
𝗡𝗔𝗥𝗨❥♡𝗝𝗔𝗔𝗧 <151159508+narendragitala@users.noreply.github.com>
2024-03-21 17:29:22 +0530

    Update play.py

--- RDXMUSIC/utils/inline/play.py
+++ RDXMUSIC/utils/inline/play.py
@@ -67,17 +67,17 @@ def stream_markup_timer(_, chat_id, played, dur):
             )
         ],
         [
-            InlineKeyboardButton(text="💜", url="https://t.me/+m4oVCt2zFhYyMTdl"),
+            InlineKeyboardButton(text="💜", url="https://t.me/JaAT_CoM_303"),
             InlineKeyboardButton(text="💚", url="https://t.me/+okCO84m2hZxjYTE9"),            
-            InlineKeyboardButton(text="💙", url="https://www.youtube.com/channel/UCoOmopJ8YVYz9Lm8iHhNYMw"),
-            InlineKeyboardButton(text="🧡", url="https://t.me/+KRt7ApiJ5Ek2ZjY1"),
+            InlineKeyboardButton(text="💙", url="https://t.me/all_about_naru"),
+            InlineKeyboardButton(text="🧡", url="https://t.me/StUdY_302"),
         ],
         [
         
             InlineKeyboardButton(
-                text="˹sᴜᴘᴘ๏ʀᴛ˼", url="https://t.me/+RObRa7kXPIJmMjU1"
+                text="˹sᴜᴘᴘ๏ʀᴛ˼", url="https://t.me/JaAT_CoM_303"
             ),
-            InlineKeyboardButton(text="˹๏ᴡɴєꝛ˼", user_id="1777270311"),           
+            InlineKeyboardButton(text="˹๏ᴡɴєꝛ˼", user_id="6441126161"),           
         ],
     ]
     return buttons
@@ -129,23 +129,23 @@ def livestream_markup(_, videoid, user_id, mode, channel, fplay):
         ],
         [
             InlineKeyboardButton(
-                text="˹sᴜᴘᴘ๏ʀᴛ˼", url="https://t.me/+PtOLQT04ocMzOTJl",
+                text="˹sᴜᴘᴘ๏ʀᴛ˼", url="https://t.me/JaAT_CoM_303",
             ),
             InlineKeyboardButton(
-                text="˹νɪᴘ ᴡᴏʀʟᴅ˼", url="https://t.me/+RObRa7kXPIJmMjU1",
+                text="˹sᴛᴜᴅʏ ᴡᴏʀʟᴅ˼", url="https://t.me/StUdY_302",
             ),
         ],
         [
             InlineKeyboardButton(
-                text="˹ᴀʙᴏᴜᴛ˼", url="https://t.me/+okCO84m2hZxjYTE9",
+                text="˹ᴀʙᴏᴜᴛ˼", url="https://t.me/all_about_naru",
                 ),
             InlineKeyboardButton(
-                text="˹๏ᴡɴєꝛ˼", user_id="1777270311",
+                text="˹๏ᴡɴєꝛ˼", user_id="6441126161",
             ),
         ],
         [
             InlineKeyboardButton(
-                text="˹ʏᴏᴜᴛᴜʙᴇ˼", url="https://www.youtube.com/channel/UCoOmopJ8YVYz9Lm8iHhNYMw",
+                text="˹ɴᴀʀᴜ˼", url="https://t.me/mr_naru",
              ),
         ],
         [
