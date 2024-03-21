794d9cf9a6a2cd1d3107ab7246d364a66ffa923c
𝗡𝗔𝗥𝗨❥♡𝗝𝗔𝗔𝗧 <151159508+narendragitala@users.noreply.github.com>
2024-03-21 17:21:02 +0530

    Update start.py

--- RDXMUSIC/utils/inline/start.py
+++ RDXMUSIC/utils/inline/start.py
@@ -10,12 +10,12 @@ def start_panel(_):
             InlineKeyboardButton(
                 text="˹sᴜᴍᴍᴏɴ ᴍє˼", url=f"https://t.me/{app.username}?startgroup=true"
             ),
-            InlineKeyboardButton(text="˹sᴜᴘᴘ๏ʀᴛ˼", url="https://t.me/+PtOLQT04ocMzOTJl"),
+            InlineKeyboardButton(text="˹sᴜᴘᴘ๏ʀᴛ˼", url="https://t.me/JaAT_CoM_303"),
         ],
         [
             InlineKeyboardButton(
                 text="˹ᴜᴘᴅᴀᴛᴇs˼",
-                url="https://t.me/+m4oVCt2zFhYyMTdl"
+                url="https://t.me/StUdY_302"
             ),
         ],
     ]
@@ -32,14 +32,14 @@ def private_panel(_):
         [
             InlineKeyboardButton(
                 text="˹sᴜᴘᴘ๏ʀᴛ˼",
-                url="https://t.me/+PtOLQT04ocMzOTJl"),
+                url="https://t.me/JaAT_CoM_303"),
             InlineKeyboardButton(
                 text="˹sᴜᴘᴘ๏ʀᴛ˼", 
-                url="https://t.me/+RObRa7kXPIJmMjU1"),],[
+                url="https://t.me/JaAT_CoM_303"),],[
             InlineKeyboardButton(text="˹sᴜᴍᴍᴏɴ ᴍє ʙᴀʙʏ˼", url=f"https://t.me/{app.username}?startgroup=true",),],
         [
-            InlineKeyboardButton(text="˹๏ᴡɴєʀ˼", user_id="1777270311"),
-            InlineKeyboardButton(text="˹ᴜᴘᴅᴀᴛєѕ˼", url="https://t.me/+m4oVCt2zFhYyMTdl"),
+            InlineKeyboardButton(text="˹๏ᴡɴєʀ˼", user_id="6441126161"),
+            InlineKeyboardButton(text="˹ᴜᴘᴅᴀᴛєѕ˼", url="https://t.me/StUdY_302"),
         ],
     ]
     return buttons
@@ -55,14 +55,14 @@ def cmd_panel(_):
         [
             InlineKeyboardButton(
                 text="˹sᴜᴘᴘ๏ʀᴛ˼",
-                url="https://t.me/+PtOLQT04ocMzOTJl"),
+                url="https://t.me/JaAT_CoM_303"),
             InlineKeyboardButton(
                 text="˹sᴜᴘᴘ๏ʀᴛ˼", 
-                url="https://t.me/+RObRa7kXPIJmMjU1"),],[
+                url="https://t.me/JaAT_CoM_303"),],[
             InlineKeyboardButton(text="˹sᴜᴍᴍᴏɴ ᴍє ʙᴀʙʏ˼", url=f"https://t.me/{app.username}?startgroup=true",),],
         [
-            InlineKeyboardButton(text="˹๏ᴡɴєʀ˼", user_id="1777270311"),
-            InlineKeyboardButton(text="˹ᴜᴘᴅᴀᴛєѕ˼", url="https://t.me/+m4oVCt2zFhYyMTdl"),
+            InlineKeyboardButton(text="˹๏ᴡɴєʀ˼", user_id="6441126161"),
+            InlineKeyboardButton(text="˹ᴜᴘᴅᴀᴛєѕ˼", url="https://t.me/StUdY_302"),
         ],
     ]
     return buttons
